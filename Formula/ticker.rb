88c440c84719a2c3bfecaedce6171a779991e6c5
Alex Li <carerley@gmail.com>
2026-08-14 23:45:14 -0400

    ticker: renamed from terminal-ticker

--- Formula/ticker.rb
+++ Formula/ticker.rb
@@ -1,8 +1,8 @@
-class TerminalTicker < Formula
+class Ticker < Formula
   desc "Quick no-key stock quotes for the terminal"
   homepage "https://github.com/carerley/terminal-ticker"
-  url "https://github.com/carerley/terminal-ticker/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3bcf3bcdb78f88f390c5930d2150a172cf8f636c28695c78c78ab936cb568315"
+  url "https://github.com/carerley/terminal-ticker/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "c73bc7cd35f896520af33052cdff793a6ddf70db64ec9115423efcf45e98afd9"
   license "MIT"
 
   depends_on "python"
@@ -21,6 +21,6 @@ class TerminalTicker < Formula
   end
 
   test do
-    assert_match "ticker 0.1.0", shell_output("#{bin}/ticker --version")
+    assert_match "ticker 0.2.0", shell_output("#{bin}/ticker --version")
   end
 end
