f9e3e255484d6253ebf9e99ad1aa5545580af29e
Alex Li <carerley@gmail.com>
2026-08-14 23:29:47 -0400

    Use available Python formula

--- Formula/terminal-ticker.rb
+++ Formula/terminal-ticker.rb
@@ -5,14 +5,14 @@ class TerminalTicker < Formula
   sha256 "3bcf3bcdb78f88f390c5930d2150a172cf8f636c28695c78c78ab936cb568315"
   license "MIT"
 
-  depends_on "python@3.14"
+  depends_on "python@3.13"
 
   def install
     libexec.install "src/ticker"
 
     launcher = libexec/"ticker-cli"
     launcher.write <<~PYTHON
-      #!#{formula_opt_bin("python@3.14")}/python3.14
+      #!#{formula_opt_bin("python@3.13")}/python3.13
       from ticker.cli import main
       raise SystemExit(main())
     PYTHON
